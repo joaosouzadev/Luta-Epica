6cbc21c567db728af34b08c28b5cf45bb2d4408a
João Victor Souza <jvms3d@gmail.com>
2018-08-21 14:49:28 -0300

    Metodo pra Logs

--- js/app.js
+++ js/app.js
@@ -4,15 +4,22 @@ new Vue({
 		vidaJogador: 100,
 		vidaMonstro: 100,
 		gameIsRunning: false,
+		turns: []
 	},
 	methods: {
 		startGame: function() {
 			this.gameIsRunning = true;
 			this.vidaJogador = 100;
 			this.vidaMonstro = 100;
+			this.turns = [];
 		},
 		ataque: function() {
-			this.vidaMonstro -= this.calculaDano(3, 14);
+			var dano = this.calculaDano(3, 14);
+			this.vidaMonstro -= dano;
+			this.turns.unshift({
+				isPlayer: true,
+				text: 'Jogador ataca Monstro e causa ' + dano + ' de dano.'
+			});
 			if (this.checaVencedor()) {
 				return;
 			}
@@ -20,7 +27,12 @@ new Vue({
 			this.ataqueMonstro();
 		},
 		ataqueEspecial: function() {
-			this.vidaMonstro -= this.calculaDano(8, 19);
+			var dano = this.calculaDano(8, 19);
+			this.vidaMonstro -= dano;
+			this.turns.unshift({
+				isPlayer: true,
+				text: 'Jogador ataca Monstro com Ataque Especial e causa ' + dano + ' de dano.'
+			});
 			if (this.checaVencedor()) {
 				return;
 			}
@@ -33,14 +45,24 @@ new Vue({
 			} else {
 				this.vidaJogador = 100;
 			}
+			this.turns.unshift({
+				isPlayer: true,
+				text: 'Jogador se cura em 10 pontos'
+			});
 			this.ataqueMonstro();
 		},
 		desistir: function() {
 			this.gameIsRunning = false;
+			this.turns = [];
 		},
 		ataqueMonstro: function() {
-			this.vidaJogador -= this.calculaDano(4, 15)
+			var dano = this.calculaDano(4, 15);
+			this.vidaJogador -= dano;
 			this.checaVencedor();
+			this.turns.unshift({
+				isPlayer: false,
+				text: 'Monstro ataca Jogador e causa ' + dano + ' de dano.'
+			});
 		},
 		calculaDano: function(min, max) {
 			return Math.max(Math.floor(Math.random() * max) + 1, min);
